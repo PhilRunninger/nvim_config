3585a060c816642b4071f2f6a779eb573caba75f
PhilRunninger <prunninger@hc-companies.com>
2023-02-27 10:09:57 -0500

    Switch to HLS color specs for statuline highlighting.

--- lua/my-statusline.lua
+++ lua/my-statusline.lua
@@ -3,7 +3,7 @@ function SetStatusLineText()
     return
         (useColor and '%1*' or '') .. " %4l/%-4L %3v " ..
         (useColor and '%2*' or '') .. "%(  %{FugitiveHead(8)} %)" ..
-        (useColor and '%3*' or '') .. "%( %{SessionNameStatusLineFlag()} %)" ..
+        (useColor and '%3*' or '') .. "%( 🕒%{SessionNameStatusLineFlag()} %)" ..
         (useColor and '%4*' or '') .. " %{&filetype} " ..
         (useColor and '%5*' or '') .. " %{&fileformat} " ..
         (useColor and '%6*' or '') .. " %(%{&readonly?'🔒':''} %)%(%{&modified?'🔴':''} %)%n " ..
@@ -12,23 +12,43 @@ end
 
 vim.opt.statusline = "%!luaeval('SetStatusLineText()')"
 
-local changeColors = function(insertMode)
-    for i = 1,6,1 do
-        local minor = vim.o.background == 'light' and  (63 + 20 * i) or  (92 - 10 * i)
-        local major = vim.o.background == 'light' and (127 + 10 * i) or (156 - 20 * i)
+local HLSToRGB = function(h,l,s)
+    -- Credit: https://www.rapidtables.com/convert/color/hsl-to-rgb.html
+    -- More explanation: https://www.wikiwand.com/en/HSL_and_HSV#To_RGB
+    local c = (1 - math.abs(2*l - 1)) * s
+    local x = c * (1 - math.abs(((h/60) % 2) - 1))
+    local m = l - c / 2
+    local r,g,b
+    if     h < 60  then r,g,b = c, x, 0
+    elseif h < 120 then r,g,b = x, c, 0
+    elseif h < 180 then r,g,b = 0, c, x
+    elseif h < 240 then r,g,b = 0, x, c
+    elseif h < 300 then r,g,b = x, 0, c
+    else                r,g,b = c, 0, x
+    end
+    return math.floor((r+m)*255), math.floor((g+m)*255), math.floor((b+m)*255)
+end
 
-        local r,b,g
-        if     vim.o.buftype == 'terminal' then r,g,b = major+64, minor+32, 0
-        elseif insertMode                  then r,g,b = 0,        minor,    major
-        elseif vim.o.modified              then r,g,b = major,    0,        0
-        else                                    r,g,b = 0,        major,    0
-        end
+local changeColors = function(insertMode)
+    -- Background Hue: terminal=gold, INSERT mode=blue, modified=red, unmodified=Green
+    local h = vim.o.buftype == 'terminal' and 55 or (insertMode and 210 or (vim.o.modified and 0 or 120))
+    -- Background Saturation
+    local s = 0.75
+    for i = 1,6,1 do
+        -- Background Luminance: Fade toward the background as i increases
+        local l = vim.o.background == 'light' and (0.25 + 0.1 * i) or (0.4 - 0.05 * i)
+        local r,g,b = HLSToRGB(h, l, s)
         local bg = 256*(256*r+g)+b
-        local luminance = 0.299*r + 0.587*g + 0.114*b
-        local fg = luminance < 128 and 0xffffff or 0
+
+        -- Foreground: h,l, and s at i=7 are from guifg of the StatusLine highlight group.
+        if vim.o.background == 'light' then
+            r,g,b = HLSToRGB(208, (-.0617 + 0.0617 * i), 0.08)
+        else
+            r,g,b = HLSToRGB(40, (1.05167 - 0.05167 * i), 0.04)
+        end
+        local fg = 256*(256*r+g)+b
         vim.cmd(string.format('highlight User%d guifg=#%06x guibg=#%06x', i, fg, bg))
     end
-    vim.cmd('highlight! link StatusLine User1')
 end
 
 local group = vim.api.nvim_create_augroup('mySLgroup', {clear = true})
