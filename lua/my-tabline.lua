a44b23f515273bb37590113bbcf474aaf595eda3
Phil Runninger <philrunninger@gmail.com>
2024-03-20 04:54:57 -0400

    Replace double width angle characters in tabline, and remove italics.

--- lua/my-tabline.lua
+++ lua/my-tabline.lua
@@ -10,14 +10,14 @@ function SetTabLine()
         local bufnr = vim.fn.tabpagebuflist(tab)[vim.fn.tabpagewinnr(tab)]
         local bufname = vim.fn.fnamemodify(vim.fn.bufname(bufnr),':t')
         tabline = tabline .. '%' .. tab .. 'T'
-        if prevIsSelected then tabline = tabline .. '%#TabLine#'    -- Selected right edge of previous tab
-        elseif isSelected then tabline = tabline .. '%#TabLineSel#' -- Selected left edge of current tab
-        elseif tab == 1 then   tabline = tabline .. '%#TabLine#'    -- Unselected left edge of 1st tab
-        else                   tabline = tabline .. '%#TabLine#'    -- Unselected right edge of previous tab
+        if prevIsSelected then tabline = tabline .. '%#TabLine#▌'    -- Selected right edge of previous tab
+        elseif isSelected then tabline = tabline .. '▐%#TabLineSel#' -- Selected left edge of current tab
+        elseif tab == 1 then   tabline = tabline .. '%#TabLine# '    -- Unselected left edge of 1st tab
+        else                   tabline = tabline .. '%#TabLine#┃'    -- Unselected right edge of previous tab
         end
-        tabline = tabline .. (vim.bo[bufnr].modified and '  ' or ' ')
-        tabline = tabline .. (bufname == '' and 'New…' or bufname) .. ' '
+        tabline = tabline .. (vim.bo[bufnr].modified and ' ' or '')
+        tabline = tabline .. (bufname == '' and 'New…' or bufname)
         prevIsSelected = isSelected
     end
-    return tabline .. (isSelected and '' or '') .. '%#TabLineFill#'
+    return tabline .. (isSelected and '%#TabLine#▌' or '▌') .. '%#TabLineFill#'
 end
