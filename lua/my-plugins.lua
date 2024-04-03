2ffbd2bee75b3b666c2fa3001b50b144abe95970
PhilRunninger <prunninger@hc-companies.com>
2024-04-02 20:09:01 -0400

    Remove rest.nvim, as it now requires a bunch of additional plugins.

--- lua/my-plugins.lua
+++ lua/my-plugins.lua
@@ -32,37 +32,6 @@ g.markdown_fenced_languages = {'vim','sql','cs','ps1'}
 cmd('packadd! vim-matchup') -- ############################# https://github.com/andymass/vim-matchup
 g.matchup_matchparen_offscreen = {method='popup'}
 
-cmd('packadd! rest.nvim') -- ################################ https://github.com/rest-nvim/rest.nvim
-require("rest-nvim").setup({
-    -- Open request results in a horizontal split
-    result_split_horizontal = false,
-    -- Keep the http file buffer above|left when split horizontal|vertical
-    result_split_in_place = false,
-    -- Skip SSL verification, useful for unknown certificates
-    skip_ssl_verification = false,
-    encode_url = true,
-    highlight = {
-        enabled = true,
-        timeout = 150,
-    },
-    result = {
-        show_url = true,
-        show_curl_command = true,
-        show_http_info = true,
-        show_headers = true,
-        formatters = {
-            json = "jq",
-            html = function(body)
-                return vim.fn.system({"tidy", "-i", "-q", "-"}, body)
-            end
-        },
-    },
-    jump_to_request = false,
-    env_file = '.env',
-    custom_dynamic_variables = {},
-    yank_dry_run = true,
-})
-
 cmd('packadd! vifm.vim') -- ######################################  https://github.com/vifm/vifm.vim
 -- vifm/vifm.vim.rc sets up mappings s, v, T, and q for quicker file picker operations.
 -- $VIFM is a vifm-internal variable that points to ~/AppData/Roaming/Vifm or ~/.config/vifm.
