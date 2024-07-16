f253042f0656928e0fe7ee32f1dba09daa4a3aa5
PhilRunninger <prunninger@hc-companies.com>
2024-07-16 12:00:37 -0400

    Update plugins, and new gitsigns setup.

--- lua/my-plugins.lua
+++ lua/my-plugins.lua
@@ -86,11 +86,11 @@ require "Comment".setup()
 cmd('packadd! gitsigns.nvim') -- ######################## https://github.com/lewis6991/gitsigns.nvim
 require "gitsigns".setup {
     signs = {
-        add = { hl = 'GitSignsAdd', text = '▊', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-        change = { hl = 'GitSignsChange', text = '▌', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-        delete = { hl = 'GitSignsDelete', text = '▎', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-        topdelete = { hl = 'GitSignsDelete', text = '▎', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-        changedelete = { hl = 'GitSignsChange', text = '▌', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+        add          = { text = '▊'},
+        change       = { text = '▌'},
+        delete       = { text = '▎'},
+        topdelete    = { text = '▎'},
+        changedelete = { text = '▌'},
     },
     on_attach = function (bufnr)
         bufmap(bufnr, 'n', ']c', ':Gitsigns next_hunk<CR>', noremapSilent)
