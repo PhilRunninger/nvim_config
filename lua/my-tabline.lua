6c74bbce2c206f24e7023b6c14dcd5bec8bfbf4e
PhilRunninger <prunninger@hc-companies.com>
2024-07-16 15:26:10 -0400

    tabline: Remove dividers between tabs.

--- lua/my-tabline.lua
+++ lua/my-tabline.lua
@@ -2,26 +2,19 @@ vim.opt.tabline = "%!luaeval('SetTabLine()')"
 
 function SetTabLine()
     local tabline = '%#Tabline#'
-    local isSelected = false
-    local prevIsSelected = false
 
     for tab = 1,vim.fn.tabpagenr('$'),1 do
-        isSelected = tab == vim.fn.tabpagenr()
-
         local window = vim.fn.tabpagewinnr(tab)
         local buftype = vim.fn.gettabwinvar(tab, window, '&buftype')
         local bufnr = vim.fn.tabpagebuflist(tab)[window]
         local bufname = vim.fn.fnamemodify(vim.fn.bufname(bufnr),':t')
 
         bufname = buftype == '' and (bufname == '' and 'New…' or bufname) or buftype
-        bufname = (vim.bo[bufnr].modified and '  ' or ' ') .. bufname .. ' '
+        bufname = ' ' .. bufname .. (vim.bo[bufnr].modified and '🔴 ' or ' ')
 
         tabline = tabline .. '%' .. tab .. 'T'
-        if tab > 1 and not prevIsSelected and not isSelected then tabline = tabline .. '|' end
-        if isSelected then bufname = '%#TabLineSel#' .. bufname .. '%#TabLine#' end
-        tabline = tabline .. bufname
-
-        prevIsSelected = isSelected
+        if tab == vim.fn.tabpagenr() then bufname = '%#TabLineSel#' .. bufname .. '%#TabLine#' end
+        tabline = tabline .. ' ' .. bufname
     end
     return tabline .. '%#TabLineFill#'
 end
