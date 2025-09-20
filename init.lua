bc160f27b81047fbcbf3b7d4ba8f92fb59473419
Phil Runninger <philrunninger@gmail.com>
2025-09-20 05:57:17 -0400

    Refactor the init.lua files. Only one is needed.

--- init.lua
+++ init.lua
@@ -1,5 +1,16 @@
 if vim.g.vscode then
-    require "code"
+    -- Use the lua/code directory for VSCode specific settings.
+    -- Example: require "code.options"
+
 else
-    require "neovim"
+    vim.cmd('colorscheme tango')
+
+    require "neovim.options"
+    require "neovim.keymaps"
+    require "neovim.deps"
+    require "neovim.autocmd"
+    require "neovim.statusline"
+    require "neovim.tabline"
+    require "neovim.floaterminal"
+    require "search"
 end
