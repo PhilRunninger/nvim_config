d55b2859dc11bd6beb30a70c57473e19af32515b
Phil Runninger <philrunninger@gmail.com>
2025-12-08 08:39:38 -0500

    Add mappings to mini.files to open files in split windows.

--- lua/neovim/deps.lua
+++ lua/neovim/deps.lua
@@ -139,6 +139,35 @@ later(function()
     })
     vim.api.nvim_set_keymap('n', '<leader>o', ':lua MiniFiles.open()<CR>', { noremap = true, silent = true })
 
+    -- Setup mappings to open files in split windows/tabs.
+    local map_split = function(buf_id, lhs, direction)
+        local rhs = function()
+            -- Make new window and set it as target
+            local cur_target = MiniFiles.get_explorer_state().target_window
+            local new_target = vim.api.nvim_win_call(cur_target, function()
+                vim.cmd(direction .. ' split')
+                return vim.api.nvim_get_current_win()
+            end)
+
+            MiniFiles.set_target_window(new_target)
+            MiniFiles.go_in()
+        end
+
+        local desc = 'Split ' .. direction
+        vim.keymap.set('n', lhs, rhs, { buffer = buf_id, desc = desc })
+    end
+
+    vim.api.nvim_create_autocmd('User', {
+        pattern = 'MiniFilesBufferCreate',
+        callback = function(args)
+            local buf_id = args.data.buf_id
+            map_split(buf_id, 's', 'horizontal')
+            map_split(buf_id, 'v', 'vertical')
+            map_split(buf_id, 't', 'tab')
+        end,
+    })
+
+    -- Create bookmarks. I wish this could be done by user, not by code.
     local set_mark = function(id, path, desc)
         MiniFiles.set_bookmark(id, path, { desc = desc })
     end
