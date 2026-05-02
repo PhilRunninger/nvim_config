3ec75edeabd0021ed88598523e5392b98be7cab7
Phil Runninger <philrunninger@gmail.com>
2026-05-02 18:12:46 -0400

    Improve and simplify search.lua.
    I briefly tried vim-interestingwords as a replacement, but I still like
    my homegrown solution better. I think this update is an improvement of
    the way it handles switching between "text" and "\<text\>".

--- lua/search.lua
+++ lua/search.lua
@@ -1,8 +1,10 @@
+-- vim:foldmethod=marker
+
 local M = {}
 
 M.words = {}
 
-local function create_search_string()
+local function create_search_string() -- {{{1
     local count = #M.words
     local msg = string.format(
         "Search [%d pattern%s]: %s",
@@ -18,62 +20,50 @@ local function create_search_string()
     end
 end
 
-function M.toggle_word(fragment, full)
-    local countBefore = #M.words
-    local fullWord = "\\<" .. fragment .. "\\>"
-    local existingFragment, existingWord
+function M.toggle_word(text) -- {{{1
     for i, word in ipairs(M.words) do
-        if word == fragment then existingFragment = i end
-        if word == fullWord then existingWord = i end
+        if word == text then
+            table.remove(M.words, i)
+            create_search_string()
+            return
+        end
+        if word == '\\<' .. text .. '\\>' or text == '\\<' .. word .. '\\>' then
+            table.remove(M.words, i)
+        end
     end
 
-    if existingFragment then
-        table.remove(M.words, existingFragment)
-    elseif existingWord then
-        table.remove(M.words, existingWord)
-    end
-
-    if (existingFragment and full)
-        or (existingWord and not full)
-        or (not existingFragment and not existingWord) then
-        table.insert(M.words, full and fullWord or fragment)
-    end
-
+    table.insert(M.words, text)
     create_search_string()
-
-    if #M.words > countBefore then
-        vim.api.nvim_feedkeys("nN", "n", false)
-    end
+    vim.fn.execute('normal! nN')
 end
 
-function M.clear_all_words()
+function M.clear_all_words() -- {{{1
     M.words = {}
     create_search_string()
 end
 
-function M.matches(pat)
+function M.matches(pattern) -- {{{1
     local buffer = vim.fn.bufnr("")
     local pos = vim.fn.winsaveview()
     vim.fn.execute('normal! gg0')
     local lines = {}
-    local nr = vim.fn.search(pat,'ceWz')
+    local nr = vim.fn.search(pattern,'ceWz')
     while nr > 0 do
-        table.insert(lines, {bufnr = buffer, lnum = nr, text = vim.fn.getline(nr):gsub('"', '\\"')})
-        nr = vim.fn.search(pat,'ceWz')
+        table.insert(lines, {bufnr = buffer, lnum = nr, text = vim.fn.getline(nr)})
+        nr = vim.fn.search(pattern,'ceWz')
     end
     vim.fn.winrestview(pos)
 
-    vim.fn.setqflist(lines)
-    vim.fn.setqflist({}, 'a', {title = pat})
+    vim.fn.setqflist(lines, 'r', {title = pattern})
     vim.cmd("copen")
 end
 
--- Keymaps
-vim.keymap.set("n", "*", function() M.toggle_word(vim.fn.expand("<cword>"), true) end, {silent=true})
-vim.keymap.set("n", "g*", function() M.toggle_word(vim.fn.expand("<cword>"), false) end, {silent=true})
+-- Keymaps {{{1
+vim.keymap.set("n", "*", function() M.toggle_word('\\<' .. vim.fn.expand("<cword>") .. '\\>') end, {silent=true})
+vim.keymap.set("n", "g*", function() M.toggle_word(vim.fn.expand("<cword>")) end, {silent=true})
 vim.keymap.set("v", "*", function()
     vim.cmd('normal! "xy')
-    M.toggle_word(vim.fn.getreg("x"), false)
+    M.toggle_word(vim.fn.getreg("x"))
 end, {silent=true})
 vim.keymap.set("n", "<leader>*", function() M.clear_all_words() end, {silent=true})
 vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>", {silent=true, noremap=true})
