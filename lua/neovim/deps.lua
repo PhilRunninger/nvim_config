23a65c96bd2821e19facc811e52cf959932314db
Phil Runninger <philrunninger@gmail.com>
2026-05-02 18:15:02 -0400

    Finally fixed the "Undefined global: 'vim'." warning. Thanks Copilot!

--- lua/neovim/deps.lua
+++ lua/neovim/deps.lua
@@ -272,34 +272,23 @@ later(function()
 
     -- Configure each language server.
     vim.lsp.config('lua_ls', {
-        on_init = function(client)
-            if client.workspace_folders then
-                local path = client.workspace_folders[1].name
-                if path ~= vim.fn.stdpath('config')
-                    and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
-                then
-                    return
-                end
-            end
-
-            client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+        settings = {
+            Lua = {
                 runtime = {
                     version = 'LuaJIT',
-                    path = {
-                        'lua/?.lua',
-                        'lua/?/init.lua',
-                    },
+                    path = vim.fn.split(package.path, ';'),
+                },
+                diagnostics = {
+                    globals = { 'vim', 'MiniDeps' },
                 },
                 workspace = {
                     checkThirdParty = false,
-                    library = {
-                        vim.env.VIMRUNTIME
-                    }
+                    library = { vim.env.VIMRUNTIME },
+                },
+                telemtry = {
+                    enable = false,
                 }
-            })
-        end,
-        settings = {
-            Lua = {}
+            }
         }
     })
 
