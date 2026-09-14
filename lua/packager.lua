80b264bce14a11bf3712e223d9ae0e70dc3fe741
Phil Runninger <philrunninger@gmail.com>
2026-09-14 12:27:31 -0400

    Make my private, per-computer rtp work with lazy.nvim.
    See my init.lua, where I set up the private path in the rtp. During
    require("lazy").setup(), the path was getting reset, so I added this to
    stop that reset.

--- lua/packager.lua
+++ lua/packager.lua
@@ -18,5 +18,6 @@ vim.opt.rtp:prepend(lazypath)
 -- Setup lazy.nvim
 require("lazy").setup({
     spec = { import = 'plugins' },
-    rocks = { enabled = false }
+    rocks = { enabled = false },
+    performance = { rtp = { reset = false } }
 })
